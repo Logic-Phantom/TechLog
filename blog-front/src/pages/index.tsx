3c3f4fe78fd9bfc662dc4201da3157434ea8281e
임채명 <dlacoaud92@naver.com>
2024-08-09 10:37:53 +0900

    메인 화면

--- blog-front/src/pages/index.tsx
+++ blog-front/src/pages/index.tsx
@@ -1,14 +1,21 @@
 import React, { FunctionComponent } from 'react'
-import Text from 'components/Text'
-import { Link } from 'gatsby'
+import styled from '@emotion/styled'
+import GlobalStyle from 'components/Common/GlobalStyle'
+import Introduction from 'components/Main/Introduction'
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+`
 
 const IndexPage: FunctionComponent = function () {
   return (
-    <div>
-      <Text text="Hello, World!" />
-      <Link to="/info/">To Info</Link>
-    </div>
-  );
-};
+    <Container>
+      <GlobalStyle />
+      <Introduction />
+    </Container>
+  )
+}
 
-export default IndexPage;
+export default IndexPage
--- blog-front/src/pages/info.tsx
+++ blog-front/src/pages/info.tsx
@@ -1,7 +1,46 @@
 import React, { FunctionComponent } from 'react'
 import { graphql } from 'gatsby'
-import Text from 'components/Text'
-import { Link } from 'gatsby'
+import { Global, css } from '@emotion/react'
+import styled from '@emotion/styled'
+
+
+
+// const TextStyle = css`
+//   font-size: 18px;
+//   font-weight: 700;
+//   color: gray;
+// `
+
+// const Text1 = styled.div`
+//   font-size: 20px;
+//   font-weight: 700;
+//   `
+
+// // Kebab Case 적용
+// const Text1 = styled.div`
+//   font-size: 20px;
+//   font-weight: 700;
+// `
+
+// // Camel Case 적용
+// const Text2 = styled('div')(() => ({
+//   fontSize: '15px',
+//   color: 'blue',
+// }))
+
+
+
+const Text1 = styled.div<{ disable: boolean }>`
+  font-size: 20px;
+  font-weight: 700;
+  text-decoration: ${({ disable }) => (disable ? 'line-through' : 'none')};
+`
+
+const Text2 = styled('div')<{ disable: boolean }>(({ disable }) => ({
+  fontSize: '15px',
+  color: 'blue',
+  textDecoration: disable ? 'line-through' : 'none',
+}))
 
 type InfoPageProps = {
   data: {
@@ -15,22 +54,29 @@ type InfoPageProps = {
   }
 }
 
+const TextStyle = css`
+  font-size: 18px;
+  font-weight: 700;
+  color: gray;
+`
+
 const InfoPage: FunctionComponent<InfoPageProps> = function ({
-    data: {
-      site: {
-        siteMetadata: { title, description, author },
-      },
+  data: {
+    site: {
+      siteMetadata: { title, description, author },
     },
-  }) {
-    return (
-      <div>
-        <Text text={title} />
-        <Text text={description} />
-        <Text text={author} />
-        <Link to="/">To Main</Link>
-      </div>
-    )
-  }
+  },
+}) {
+  return (
+    <div>
+      <Global styles={globalStyle} />
+      <div css={TextStyle}>{title}</div>
+      <Text1 disable={true}>{description}</Text1>
+      <Text2 disable={true}>{author}</Text2>
+    </div>
+  )
+}
+
 
 export default InfoPage
 
