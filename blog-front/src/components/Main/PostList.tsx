aa9854714f7863ab7d9801f0669cebfa676cec17
임채명 <dlacoaud92@naver.com>
2024-08-12 10:09:28 +0900

    반응형 추가

--- blog-front/src/components/Main/PostList.tsx
+++ blog-front/src/components/Main/PostList.tsx
@@ -3,11 +3,11 @@ import styled from '@emotion/styled'
 import PostItem from 'components/Main/PostItem'
 
 const POST_ITEM_DATA = {
-  title: 'Post Item Title',
-  date: '2020.01.29.',
+  title: 'React 시작하기',
+  date: '2024.08.12.',
   categories: ['Web', 'Frontend', 'Testing'],
   summary:
-    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident repellat doloremque fugit quis rem temporibus! Maxime molestias, suntrem debitis odit harum impedit. Modi cupiditate harum dignissimos eos in corrupti!',
+    'React를 사용하여 블로그 만들기 시작',
   thumbnail:
     'https://github.com/codemasterli/TechLog/blob/main/blog-front/contents/react.png?raw=true',
   link: 'https://www.google.co.kr',
@@ -20,14 +20,17 @@ const PostListWrapper = styled.div`
   width: 768px;
   margin: 0 auto;
   padding: 50px 0 100px;
+
+  @media (max-width: 768px) {
+    grid-template-columns: 1fr;
+    width: 100%;
+    padding: 50px 20px;
+  }
 `
 //실질적인 아이템 목록 
 const PostList: FunctionComponent = function () {
   return (
     <PostListWrapper>
-      <PostItem {...POST_ITEM_DATA} />
-      <PostItem {...POST_ITEM_DATA} />
-      <PostItem {...POST_ITEM_DATA} />
       <PostItem {...POST_ITEM_DATA} />
     </PostListWrapper>
   )
