83ca4c9110fed3f4f4d3ac5942fc4aebd5f01abb
임채명 <dlacoaud92@naver.com>
2024-08-12 15:45:01 +0900

    MarkDown 게시글 기능 추가

--- blog-front/src/templates/post_template.tsx
+++ blog-front/src/templates/post_template.tsx
@@ -3,6 +3,7 @@ import { graphql } from 'gatsby'
 import { PostPageItemType } from 'types/PostItem.types' // 바로 아래에서 정의할 것입니다
 import Template from 'components/Common/Template'
 import PostHead from 'components/Post/PostHead'
+import PostContent from 'components/Post/PostContent'
 
 
 type PostTemplateProps = {
@@ -23,7 +24,7 @@ type PostTemplateProps = {
         html,
         frontmatter: {
           title,
-          summary, // 나중에 사용할 예정입니다!
+          summary,
           date,
           categories,
           thumbnail: {
@@ -41,6 +42,7 @@ type PostTemplateProps = {
           categories={categories}
           thumbnail={gatsbyImageData}
         />
+        <PostContent html={html} />
       </Template>
     )
   }
